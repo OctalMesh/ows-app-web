2561d480662b537c84978649c4992dbda912b7b1
Nykon Hrytsyshyn <contact@nykon.me>
2026-02-22 18:45:41 +0200

    Init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import OctalMeshLogo from "@/app/components/OctalMeshLogo";
+
+export default function Home() {
+  return (
+      <div className="flex min-h-screen items-center justify-center px-10">
+        <OctalMeshLogo className="w-130" />
+      </div>
+  );
+}
